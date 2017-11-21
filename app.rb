b9cabac7d674f549c12a0a1c77bebbd8f3c7ca51
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-21 23:41:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Trever Andersen"
+  end
+
+  get '/hometown' do
+    "My hometown is Elk Horn, IA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is By Design - Kid Cudi, Andre Benjamin"
+  end
+
 end
